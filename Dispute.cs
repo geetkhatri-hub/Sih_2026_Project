7e5199cf117e491b6ee85a4e5dba05da04b123d9
geetkhatri-hub <geetkhatri333@gmail.com>
2026-08-16 14:32:05 +0530

    Add files via upload

---
+++ Dispute.cs
@@ -0,0 +1,12 @@
+﻿namespace SIH_2026.Models
+{
+    public class Dispute
+    {
+        public int ID { get; set; }
+        public int TripId { get; set; }
+        public string Category { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Status { get; set; } = "submitted";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
